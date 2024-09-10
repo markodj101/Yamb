375dc7ebb67c47b9b99d09f3fa3884cb209eeb5b
markodj101 <MarkoDj10@gmail.com>
2024-09-10 17:34:44 +0200

    Promjean u YambServer.java

--- server/YambServer.java
+++ server/YambServer.java
@@ -3,5 +3,6 @@ package server;
 public class YambServer {
     public static void main(String[] args) {
         System.out.println("Yamb Server srdjan");
+        System.out.println("Drugi poksuaj");
     }
 }
